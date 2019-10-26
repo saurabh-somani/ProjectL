fd208c36cecdee9c2f3bd4180fc52b21cc69530f
saurabh-somani <ssomani.saurabh@gmail.com>
2019-10-26 13:28:13 +0530

    hehe

--- ProjectL/Student.aspx.cs
+++ ProjectL/Student.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.Sql;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace ProjectL
@@ -14,13 +15,13 @@ namespace ProjectL
         
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            //Select();
         }
 
         protected void Login(object sender, EventArgs e)
         {
             SqlConnection con = new SqlConnection();
-            con.ConnectionString = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = 'E:\IT Lab project\myrepo\ProjectL\PlacementDB.mdf'; Integrated Security = True; Pooling = False";
+            con.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;AttachDbFilename='C:\Users\Mahe\Desktop\7th Sem\IT_LAB\Codes\Project\repo\ProjectL\ProjectL\PlacementDB.mdf';Integrated Security=True";
             try
             {
                 con.Open();
@@ -47,6 +48,31 @@ namespace ProjectL
             {
                 Label1.Text = "Error " + ex.ToString();
             }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        protected void Select()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;AttachDbFilename='PlacementDB.mdf';Initial Catalog=PlacementDB;Integrated Security=True";
+
+            SqlCommand com = new SqlCommand("SELECT * FROM Offers", con);
+
+            try
+            {
+                con.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(com);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds, "Offer");
+
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+            }
+            catch(Exception)
+            { }
             finally
             {
                 con.Close();
