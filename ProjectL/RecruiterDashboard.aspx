44fcb27223132262180c7e94328f69f13392935c
saurabh-somani <ssomani.saurabh@gmail.com>
2019-10-28 19:48:35 +0530

    RecruiterDashboard page added

---
+++ ProjectL/RecruiterDashboard.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="RecruiterDashboard.aspx.cs" Inherits="ProjectL.RecruiterDashboard" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style type="text/css">
+    .auto-style1 {
+        width: 500px;
+    }
+    .auto-style2 {
+        width: 100px;
+    }
+    .auto-style3 {
+        width: 212px;
+    }
+</style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="LeftContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="CenterContent" runat="server">
+    <p>
+    Recruiter Dashboard:</p>
+<p>
+    &nbsp;</p>
+<table class="auto-style1">
+    <tr>
+        <td class="auto-style2">Company:</td>
+        <td class="auto-style3">
+            <asp:DropDownList ID="DropDownList1" runat="server" DataSourceID="SqlDataSource1" DataTextField="Company" DataValueField="Company" Width="126px">
+            </asp:DropDownList>
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:PlacementDBConnectionString %>" SelectCommand="SELECT DISTINCT [Company] FROM [Company] ORDER BY [Company]"></asp:SqlDataSource>
+        </td>
+        <td>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="DropDownList1" ErrorMessage="RequiredFieldValidator"></asp:RequiredFieldValidator>
+        </td>
+        <td>&nbsp;</td>
+    </tr>
+    <tr>
+        <td class="auto-style2">SubType:</td>
+        <td class="auto-style3">
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        </td>
+        <td>
+            &nbsp;</td>
+        <td>&nbsp;</td>
+    </tr>
+    <tr>
+        <td class="auto-style2">E-mail ID:</td>
+        <td class="auto-style3">
+            <asp:TextBox ID="TextBox3" runat="server" TextMode="Email"></asp:TextBox>
+        </td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+    </tr>
+    <tr>
+        <td class="auto-style2">Phone:</td>
+        <td class="auto-style3">
+            <asp:TextBox ID="TextBox4" runat="server" TextMode="Number"></asp:TextBox>
+        </td>
+        <td>
+            <asp:RangeValidator ID="RangeValidator1" runat="server" ControlToValidate="TextBox4" ErrorMessage="RangeValidator" MaximumValue="9999999999" MinimumValue="1000000000" Type="Double"></asp:RangeValidator>
+        </td>
+        <td>&nbsp;</td>
+    </tr>
+</table>
+<br />
+<asp:Button ID="Update" runat="server" Text="Update" OnClick="Update_Click" />
+&nbsp;&nbsp;&nbsp;
+    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+</asp:Content>
---
+++ ProjectL/RecruiterDashboard.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjectL
+{
+    public partial class RecruiterDashboard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void UpdateQuery()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["PlacementDB"].ConnectionString;
+            SqlCommand com = null;
+            String query = "";
+            if(TextBox2.Text != null)
+            {
+                query = "UPDATE Company SET ";
+                query += "SubType = @st ";
+                query += "WHERE Company = @c";
+                com = new SqlCommand(query, con);
+                com.Parameters.AddWithValue("@c", DropDownList1.SelectedItem.Text);
+                com.Parameters.AddWithValue("@st", TextBox2.Text);
+
+                try
+                {
+                    con.Open();
+                    com.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("<script>alert(" + ex.ToString() + ")</script>");
+                }
+                finally
+                {
+                    com.Dispose();
+                    con.Close();
+                }
+            }
+            if(TextBox3.Text != null)
+            {
+                query = "UPDATE Company SET ";
+                query += "email = @e ";
+                query += "WHERE Company = @c";
+                com = new SqlCommand(query, con);
+                com.Parameters.AddWithValue("@c", DropDownList1.SelectedItem.Text);
+                com.Parameters.AddWithValue("@e", TextBox3.Text);
+
+                try
+                {
+                    con.Open();
+                    com.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("<script>alert(" + ex.ToString() + ")</script>");
+                }
+                finally
+                {
+                    com.Dispose();
+                    con.Close();
+                }
+            }
+            if(TextBox4.Text != null)
+            {
+                query = "UPDATE Company SET ";
+                query += "phone = @p ";
+                query += "WHERE Company = @c";
+                com = new SqlCommand(query, con);
+                com.Parameters.AddWithValue("@c", DropDownList1.SelectedItem.Text);
+                com.Parameters.AddWithValue("@p", TextBox4.Text);
+
+                try
+                {
+                    con.Open();
+                    com.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("<script>alert(" + ex.ToString() + ")</script>");
+                }
+                finally
+                {
+                    com.Dispose();
+                    con.Close();
+                }
+            }
+        }
+
+        protected void Update_Click(object sender, EventArgs e)
+        {
+            UpdateQuery();
+            Label1.Text = "Updated Successfully";
+        }
+    }
+}
