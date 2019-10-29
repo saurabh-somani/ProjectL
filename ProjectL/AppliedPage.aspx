85a0816a7e37c2b37071bdd92d06e73a67f8b097
akshit-akhoury <akshit.akhoury@gmail.com>
2019-10-29 11:50:01 +0530

    New Page code

---
+++ ProjectL/AppliedPage.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="AppliedPage.aspx.cs" Inherits="ProjectL.AppliedPage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="LeftContent" runat="server">
+    <asp:Button ID="Button2" runat="server" Text="Companies" onclick="Companies"/>
+    <br />
+
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="CenterContent" runat="server">
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false" AutoGenerateSelectButton="true" OnSelectedIndexChanged="info">
+        <Columns>
+            <asp:BoundField DataField="OfferID" HeaderText ="OfferID" />
+            <asp:BoundField DataField ="Company" HeaderText ="Company" />
+            <asp:BoundField DataField ="Role" HeaderText="Role" />
+        </Columns>
+
+    </asp:GridView>
+</asp:Content>
---
+++ ProjectL/AppliedPage.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Data;
+
+namespace ProjectL
+{
+    public partial class AppliedPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["PlacementDB"].ConnectionString;
+
+            SqlCommand com = new SqlCommand("SELECT o.OfferID, Company, Role FROM Offer o INNER JOIN Applied a ON o.OfferID = a.OfferID AND a.StudentID = @s", con);
+            com.Parameters.AddWithValue("@s", Session["user"].ToString());
+            
+            try
+            {
+                con.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(com);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds, "Offer");
+
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+            }
+            catch (Exception ex)
+            {
+                String s = ex.ToString();
+                Response.Write("<script>alert(" + s + ")</script>");
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        protected void Companies(object sender, EventArgs e)
+        {
+            Response.Redirect("StudentPage.aspx");
+        }
+
+        protected void info(object sender, EventArgs e)
+        {
+            string s;
+            s = "InfoPage.aspx?OfferID=" + GridView1.SelectedRow.Cells[1].Text;
+            Response.Redirect(s);
+        }
+    }
+}
